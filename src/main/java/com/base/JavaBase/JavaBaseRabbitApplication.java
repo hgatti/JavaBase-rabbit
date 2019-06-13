34ef29582b4979931a526de3f0c0985f57614447
Hector Frandini Gatti <hfgatti@uolinc.com>
2019-06-13 11:53:45 -0300

    Commit Inicial

---
+++ src/main/java/com/base/JavaBase/JavaBaseRabbitApplication.java
@@ -0,0 +1,15 @@
+package com.base.JavaBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class JavaBaseRabbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBaseRabbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/base/JavaBase/JavaBaseRabbitApplicationTests.java
@@ -0,0 +1,16 @@
+//package com.base.JavaBase;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class JavaBaseRabbitApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
